05b5781672e8a8691b8941fae0fdb823d2cc13b8
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-08-19 06:54:24 -0700

    set up the bakend server file

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import movies from './api/movies.route.js';
+
+// create the server
+const app = express();
+
+// attach cors and express.json middleware
+app.use(cors());
+app.use(express.json());
+
+// specify the initial routes
+app.use('/api/v1/movies', movies);
+app.use('*', (req, res) => {
+  res.status(404).json({ error: 'not found' });
+});
+
+export default app;
