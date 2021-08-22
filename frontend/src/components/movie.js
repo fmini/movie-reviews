55c45c6a208c6ec16092af0652339ce2d8bec1fa
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-08-22 16:46:45 -0700

    added list reviews functionality to movies component

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -6,8 +6,9 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Butto';
+import Button from 'react-bootstrap/Button';
 import Media from 'react-bootstrap/Media';
+import moment from 'moment';
 
 const Movie = props => {
   const [movie, setMovie] = useState({
@@ -26,11 +27,12 @@ const Movie = props => {
       .catch(e => {
         console.log(e);
       });
-
-    useEffect(() => {
-      getMovie(props.match.params.id);
-    }, [props.match.params.id]);
   };
+
+  useEffect(() => {
+    getMovie(props.match.params.id);
+  }, [props.match.params.id]);
+
   return (
     <div>
       <Container>
@@ -52,6 +54,38 @@ const Movie = props => {
             </Card>
             <br></br>
             <h2>Reviews</h2>
+            <br></br>
+            {movie.reviews.map((review, index) => {
+              return (
+                <Media key={index}>
+                  <Media.Body>
+                    <h5>
+                      {review.name + 'reviewed on '}{' '}
+                      {moment(review.date).format('Do MMMM YYYY')}
+                    </h5>
+                    <p>{review.review}</p>
+                    {props.user && props.user.id === review.user.id && (
+                      <Row>
+                        <Col>
+                          <Link
+                            to={{
+                              pathname:
+                                '/movies' + props.match.params.id + '/review',
+                              state: { currentReview: review },
+                            }}
+                          >
+                            Edit
+                          </Link>
+                        </Col>
+                        <Col>
+                          <Button variant="link">Delete</Button>
+                        </Col>
+                      </Row>
+                    )}
+                  </Media.Body>
+                </Media>
+              );
+            })}
           </Col>
         </Row>
       </Container>
