982a5b72791d3444394e0b026406c019ad8e46c3
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-08-22 10:11:13 -0700

    Added and modified react-bootstrap navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,43 @@
+import { React, useState } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddReview from './components/add-review';
 import Login from './components/login';
 import Movie from './components/movie';
 import MoviesList from './components/movies-list';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+  async function logout() {
+    setUser(null);
+  }
+
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand> Movie Reviews </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to={'/movies'}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={'/Login'}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Login />
       <AddReview />
       <MoviesList />
